2376e097355f6ff5af6b520c3fc1cf8e0012a3c2
Prem kadgaonkar <145741253+Premkadgaonkar@users.noreply.github.com>
2026-08-14 21:52:58 +0530

    [637] [Time Beats: 43.31%] [Memory Beats: 14.63%] - LeetPush

---
+++ 637-Average-of-Levels-in-Binary-Tree.py
@@ -0,0 +1,56 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Queue:
+    def __init__(self):
+        self.q=[]
+        self.front=-1
+    def enqueue(self,x):
+        if self.front==-1:
+            self.front=0
+        self.q.append(x)
+    def dequeue(self):
+        if len(self.q)==0:
+            return -1
+        x=self.q[self.front]
+        self.front+=1
+        if self.front==len(self.q):
+            self.front=-1
+            self.q=[]
+        return x
+    def size(self):
+        if self.front==-1:
+            return 0
+        return len(self.q)-self.front     
+
+        
+
+class Solution:
+    def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        ans=[]
+        if root is None:
+            return ans
+        q=Queue()
+        q.enqueue(root)
+        ans.append((root.val)/1)
+
+        while q.size()>0:
+            l=q.size()
+            level=[]
+            for i in range(l):
+                front=q.dequeue()
+                if front.left!=None:
+                    q.enqueue(front.left)
+                    level.append(front.left.val)
+                if front.right!=None:
+                    q.enqueue(front.right)
+                    level.append(front.right.val)
+
+            if len(level)!=0:
+                ans.append(sum(level)/len(level))
+        
+        return ans
+        
