93abcaae6598a124e607419d503a79419f49e1bb
Prem kadgaonkar <145741253+Premkadgaonkar@users.noreply.github.com>
2026-08-14 21:42:53 +0530

    [103] [Time Beats: 100.00%] [Memory Beats: 14.63%] - LeetPush

---
+++ 103-Binary-Tree-Zigzag-Level-Order-Traversal.py
@@ -0,0 +1,61 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Queue:
+    def __init__(self):
+        self.q=[]
+        self.front=-1
+    def enqueue(self,x):
+        if self.front==-1:
+            self.front=0
+        self.q.append(x)
+    def dequeue(self):
+        if len(self.q)==0:
+            return -1
+        x=self.q[self.front]
+        self.front+=1
+        if self.front==len(self.q):
+            self.front=-1
+            self.q=[]
+        return x
+    def size(self):
+        if self.front==-1:
+            return 0
+        return len(self.q)-self.front     
+
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        ans=[]
+        if root is None:
+            return ans
+        q=Queue()
+        q.enqueue(root)
+        ans.append([root.val])
+        c=0
+
+        while q.size()>0:
+            l=q.size()
+            level=[]
+            for i in range(l):
+                front=q.dequeue()
+                if front.left!=None:
+                    q.enqueue(front.left)
+                    level.append(front.left.val)
+                if front.right!=None:
+                    q.enqueue(front.right)
+                    level.append(front.right.val)
+            
+            c+=1
+            
+            if len(level)!=0:
+                if c%2==0:
+                    ans.append(level)
+                else:
+                    ans.append(level[-1::-1])
+        
+        return ans
+        
