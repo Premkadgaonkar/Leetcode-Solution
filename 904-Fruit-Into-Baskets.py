be1117104f206605fd8fa10a226a4e9228f6d51b
Prem kadgaonkar <145741253+Premkadgaonkar@users.noreply.github.com>
2026-08-29 10:28:46 +0530

    [904] [Time Beats: 47.60%] [Memory Beats: 13.48%] - LeetPush

---
+++ 904-Fruit-Into-Baskets.py
@@ -0,0 +1,20 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        ans=0
+        l=0
+        freq={}
+        for i in range(len(fruits)):
+            freq[fruits[i]] = freq.get(fruits[i], 0) + 1
+
+            while len(freq)>2:
+                freq[fruits[l]]-=1
+                if freq[fruits[l]]==0:
+                    del freq[fruits[l]]
+
+                l+=1
+            
+            ans=max(ans,i-l+1)
+
+        return ans
+
+            
