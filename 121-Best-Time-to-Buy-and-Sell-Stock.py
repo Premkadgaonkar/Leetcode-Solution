b1a8149daf08d76eee9e19b518ebea5756606a51
Prem kadgaonkar <145741253+Premkadgaonkar@users.noreply.github.com>
2026-09-09 11:31:12 +0530

    [121] [Time Beats: 34.93%] [Memory Beats: 42.15%] - LeetPush

---
+++ 121-Best-Time-to-Buy-and-Sell-Stock.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        mini=prices[0]
+        ans=0
+        for i in range(1,len(prices)):
+            ans=max(ans,prices[i]-mini)
+            mini=min(mini,prices[i])
+        return ans
+        
